8149aebfeaabe9fd1abf93eb241fde225dea167f
Adam Morelli <morelli@3cisd.com>
2022-11-14 22:10:44 -0500

    Exercise 1.3

--- 1.1/1.3.scm
+++ 1.1/1.3.scm
@@ -1,8 +1,19 @@
-; exercise 1.3
 ; To load: 'csi' to start CHICKEN at cmd line
 ;   then (load "1.3.scm")
 
-; (define (square x) (* x x))
+; exercise 1.3
+; Define a procedure that takes 3 inputs, and sum of squares the two larger numbers
 
-; (display ":)test.scm has been loaded\n:)functions available: (square (x))\n")
+(define (square x) 
+    (* x x))
 
+(define (sum-square i j) 
+    (+ (square i) (square j)))
+
+(define (sum-square-largest a b c)
+    (cond 
+        ((and (>= a b) (>= c b)) (sum-square a c))
+        ((and (>= b a) (>= c a)) (sum-square b c))
+        (else (sum-square a b))
+    )
+)
