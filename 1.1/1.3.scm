7902c955da645100ecb922aeef2498bc9f68a578
Adam Morelli <morelli@3cisd.com>
2022-11-14 21:21:51 -0500

    initial commit

---
+++ 1.1/1.3.scm
@@ -0,0 +1,8 @@
+; exercise 1.3
+; To load: 'csi' to start CHICKEN at cmd line
+;   then (load "1.3.scm")
+
+; (define (square x) (* x x))
+
+; (display ":)test.scm has been loaded\n:)functions available: (square (x))\n")
+
